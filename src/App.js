4c85ad188c4c4011db3f27c1941bbd8cba5af8ab
vincentchandra <vincentchristian6@gmail.com>
2020-08-03 20:03:38 +0700

    menambahkan tugas11 dan react app baru

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FoodTable from "./tugas11/FoodTable"
+
+function App() {
+  return (
+    <FoodTable />
+  );
+} 
+
+export default App;
---
+++ src/tugas11/FoodTable.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import "./FoodTable.css";
+class Title extends Component{
+    render(){
+        return (
+            <>
+             <h1 style={{textAlign:"center", width:"800px",margin:"50px"}}>{this.props.title}</h1>
+            </>
+        )
+    }
+}
+let dataHargaBuah = [
+    {nama: "Semangka", harga: 10000, berat: 1000},
+    {nama: "Anggur", harga: 40000, berat: 500},
+    {nama: "Strawberry", harga: 30000, berat: 400},
+    {nama: "Jeruk", harga: 30000, berat: 1000},
+    {nama: "Mangga", harga: 30000, berat: 500}
+]
+class Table extends Component{
+    render(){
+        return(
+            
+            <table>
+                <tr>
+                    <th>Nama</th>
+                    <th>Harga</th>
+                    <th>Berat</th>
+                </tr>
+                {dataHargaBuah.map(el => {
+                    return(
+                        <tr>
+                            <td style={{textAlign:"left"}}>{el.nama}</td>
+                            <td style={{textAlign:"left"}}>{el.harga}</td>
+                            <td style={{textAlign:"left"}}>{el.berat/1000} kg</td>
+                        </tr>
+                    )
+                })}
+                
+            </table>
+        )
+    }
+}
+function FoodTable(){
+    return(
+        <>
+            <Title title="Tabel Harga Buah"/>
+            <Table/>
+        </>
+    )
+}
+
+export default FoodTable;
